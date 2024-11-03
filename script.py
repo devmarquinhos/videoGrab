99eca417347534a7ea7e8ae65e6b651df1ac7950
devmarquinhos <marcos.emanuel.04@gmail.com>
2024-11-03 15:07:37 -0300

    :sparkles: added options to the user

--- script.py
+++ script.py
@@ -1,10 +1,26 @@
 from pytubefix import YouTube
 
 link = str(input("Video URL > "))
+decision = ""
 
 if link != "":
     yt = YouTube(link)
-    stream = yt.streams.get_highest_resolution().download()
+
+    while decision == "":
+        print("1 - Get highest quality\n2 - Download audio")
+        decision = str(input())
+        match decision:
+            case "1":
+                print("Downloading video in highest quality.")
+                streams = yt.streams.get_highest_resolution().download()
+            case "2":
+                print("Downloading the audio from the video.")
+                streams = yt.streams.get_audio_only().download(mp3=True)
+            case _:
+                print("Invalid option")    
+                break 
+        decision = ""
+        
 else:
     print("Invalid URL")
     
